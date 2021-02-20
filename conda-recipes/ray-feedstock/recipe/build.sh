d650a61261b19d7015fda6cc3059b67c16eda1df
Miguel de Benito Delgado <debenito@unternehmertum.de>
2021-02-20 00:05:31 +0000

    Remove silly check and use env

--- conda-recipes/ray-feedstock/recipe/build.sh
+++ conda-recipes/ray-feedstock/recipe/build.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
 export SKIP_PYARROW_INSTALL=True
+export SKIP_THIRDPARTY_INSTALL=True
 cd python
 cd ray/dashboard/client && npm ci && npm run build
 cd ../../..
 $PYTHON setup.py install --single-version-externally-managed --record=record.txt
 
-# Allow Conda to modify rpath in files
+# Allow conda to modify rpath in files
 chmod -R u+w $PREFIX 
